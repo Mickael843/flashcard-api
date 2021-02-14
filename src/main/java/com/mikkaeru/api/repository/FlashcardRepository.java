7d3018b9d506be9916799b7e24bcddc32fceda86
Mickael Luiz Machado <mickaelluiz843@gmail.com>
2021-02-13 21:59:08 -0300

    alterando repositorios

--- src/main/java/com/mikkaeru/api/repository/FlashcardRepository.java
+++ src/main/java/com/mikkaeru/api/repository/FlashcardRepository.java
@@ -1,4 +1,25 @@
 package com.mikkaeru.api.repository;
 
+import com.mikkaeru.api.domain.model.flashcard.Flashcard;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface FlashcardRepository {
+
+    String ID_FIELD = "id";
+    String BOX_FIELD = "box";
+    String BACK_FIELD = "back";
+    String FRONT_FIELD = "front";
+    String REVIEW_FIELD = "review";
+    String CREATED_AT_FIELD = "created_at";
+    String UPDATED_AT_FIELD = "updated_at";
+    String EXTERNAL_ID_FIELD = "external_id";
+    String LAST_REVISION_FIELD = "last_revision";
+
+    void create(Flashcard flashcard);
+
+    void update(Flashcard flashcard);
+
+    Optional<Flashcard> findByExternalId(UUID externalId);
 }
--- src/main/java/com/mikkaeru/api/repository/impl/FlashcardRepositoryImpl.java
+++ src/main/java/com/mikkaeru/api/repository/impl/FlashcardRepositoryImpl.java
@@ -2,7 +2,12 @@ package com.mikkaeru.api.repository.impl;
 
 import com.mikkaeru.api.domain.model.flashcard.Flashcard;
 import com.mikkaeru.api.repository.FlashcardRepository;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public class FlashcardRepositoryImpl implements FlashcardRepository {
 
@@ -12,12 +17,42 @@ public class FlashcardRepositoryImpl implements FlashcardRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Flashcard create(Flashcard entity) {
 
-        var sql = "";
+    @Override
+    public void create(Flashcard flashcard) {
 
-        jdbcTemplate.update(sql);
+        var sql = "INSERT INTO flashcard(" + BOX_FIELD + ", " + BACK_FIELD + ", " + FRONT_FIELD + ", " + REVIEW_FIELD +
+                ", " + EXTERNAL_ID_FIELD + ", " + CREATED_AT_FIELD + ", " + UPDATED_AT_FIELD + ", " + LAST_REVISION_FIELD +
+                ") " + "VALUES (" + BOX_FIELD + ", " + BACK_FIELD + ", " + FRONT_FIELD + ", " + REVIEW_FIELD + ", " + EXTERNAL_ID_FIELD +
+                ", " + CREATED_AT_FIELD + ", " + UPDATED_AT_FIELD + ", " + LAST_REVISION_FIELD + ")";
 
-        return null;
+        try {
+
+            MapSqlParameterSource parameters = new MapSqlParameterSource()
+                    .addValue(BOX_FIELD, flashcard.getBox())
+                    .addValue(BACK_FIELD, flashcard.getBack())
+                    .addValue(FRONT_FIELD, flashcard.getFront())
+                    .addValue(REVIEW_FIELD, flashcard.getReview())
+                    .addValue(EXTERNAL_ID_FIELD, flashcard.getExternalId())
+                    .addValue(CREATED_AT_FIELD, flashcard.getCreatedAt())
+                    .addValue(UPDATED_AT_FIELD, flashcard.getUpdatedAt())
+                    .addValue(LAST_REVISION_FIELD, flashcard.getLastRevision());
+
+            jdbcTemplate.update(sql, parameters);
+
+        } catch (DuplicateKeyException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void update(Flashcard flashcard) {
+
+    }
+
+    @Override
+    public Optional<Flashcard> findByExternalId(UUID externalId) {
+        return Optional.empty();
     }
 }
